d7b7176906eb00b06717d88d35467dfeda06d985
Thabo Matjuda <tl.matjuda@gmail.com>
2024-03-18 16:28:05 +0400

    Migrated Maven as well now to the Commons.

---
+++ maven/mavenutil.go
@@ -0,0 +1,57 @@
+package maven
+
+import (
+	"github.com/tlmatjuda/toob-commons/cli"
+	"github.com/tlmatjuda/toob-commons/text"
+	"strings"
+)
+
+const (
+	Clean                  = "c"
+	Install                = "i"
+	Offline                = "o"
+	Tests                  = "t"
+	SkipTests              = "-DskipTests=true"
+	SingleThreadPerCPUCore = "-T 1C"
+)
+
+func ExtractMavenArgs(args []string) (string, string) {
+	targetPath := text.GetArg(args, "-dir")
+	mavenBuildOptions := text.GetArg(args, "-opts")
+	return targetPath, mavenBuildOptions
+}
+
+func Run(targetPath string, mavenOptions string, captureCmdOutput bool, logFilePath string) string {
+	var consoleResponse string
+	var mavenCliFlags []string
+
+	if strings.Contains(mavenOptions, Clean) {
+		mavenCliFlags = append(mavenCliFlags, "clean")
+	}
+
+	if strings.Contains(mavenOptions, Install) {
+		mavenCliFlags = append(mavenCliFlags, "install")
+	}
+
+	if strings.Contains(mavenOptions, Offline) {
+		mavenCliFlags = append(mavenCliFlags, "-o")
+	}
+
+	if !strings.Contains(mavenOptions, Tests) {
+		mavenCliFlags = append(mavenCliFlags, SkipTests)
+	}
+
+	if text.StringNotBlank(logFilePath) {
+		mavenCliFlags = append(mavenCliFlags, "-log-file="+logFilePath)
+	}
+
+	mavenCliFlags = append(mavenCliFlags, SingleThreadPerCPUCore)
+
+	if captureCmdOutput {
+		consoleResponse = cli.Exec("mvn", mavenCliFlags, targetPath, captureCmdOutput)
+	} else {
+		cli.Exec("mvn", mavenCliFlags, targetPath, captureCmdOutput)
+	}
+
+	return consoleResponse
+}
